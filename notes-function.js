17e5d551ee2f4dee611042db79d60a0b6247f7ee
SammyK08 <samrajkarki08@gmail.com>
2019-09-05 00:16:27 -0400

    updating the sorting button
    We sort the note either by created date or by updated date or alpbhanetically. we already had implemented the sorting by updated date. In this part we added the sorting by date created and alphabetically

--- notes-function.js
+++ notes-function.js
@@ -58,7 +58,29 @@ if(sortBy==='byEdited'){
           return 0
        }
     })
-}else{
+}else if(sortBy==='byCreated'){
+   return notes.sort(function(a,b){
+      if(a.createdAt>b.createdAt){
+         return -1
+      }else if(a.createdAt<b.createdAt){
+         return 1
+      }
+      else{
+         return 0
+      }
+   })
+}else if(sortBy==='alphabetical'){
+ return notes.sort(function(a,b){
+    if(a.title.toLowerCase()<b.title.toLowerCase()){
+       return -1
+    }else if(a.title.toLowerCase()>b.title.toLowerCase()){
+       return 1
+    }else{
+       return 0
+    }
+ })
+}
+else{
    return notes
 }
 
